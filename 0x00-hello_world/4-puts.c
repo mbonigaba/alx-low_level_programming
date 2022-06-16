1078eca5deb666804f1daf47ac9bbde34fca9294
mbonigaba <yvesishi3@gmail.com>
2022-06-16 08:46:27 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * *main - Entry point
- * *My hello, printf task
- * *Return: return 1 after program execution
- * */
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
