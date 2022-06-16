f1002f7bfc311e9e2e4f391347129cccee2256ba
mbonigaba <yvesishi3@gmail.com>
2022-06-16 09:22:59 -0700

    my 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,29 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Sucess/correct)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 
 {
-	char lower_case;
-	char upper_case;
+	char alphabet;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		putchar(lower_case);
+		putchar(alphabet);
 	}
 
-
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{
-		putchar(upper_case);
-	}
-
-
-
-
-	putchar ('\n');
+	putchar('\n');
 
 	return (0);
 
